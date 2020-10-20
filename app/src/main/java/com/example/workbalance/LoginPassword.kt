347a90494d3b0ddd183fbd29e3457b7576e0522f
Edvin <Edvinbe@student.chalmers.se>
2020-10-20 16:13:04 +0200

    Closing keyboard and adding comments

--- app/src/main/java/com/example/workbalance/LoginPassword.kt
+++ app/src/main/java/com/example/workbalance/LoginPassword.kt
@@ -36,22 +36,30 @@ class LoginPassword : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        // Populating account selection spinner
+        // TODO select same account as on first page.
         val work: AccountModel = AccountModel("Work@work.com", "ic_account_circle")
         val private: AccountModel = AccountModel("Private@home.com", "ic_account_circle")
 
         val modelList: List<AccountModel> = listOf(work, private)
-
         val customDropDownAdapter = context?.let { CustomDropDownAdapter(it, modelList) }
         spinner04.adapter = customDropDownAdapter
 
+        // Requesting focus to password field and opening keyboard
         editTextTextPassword.requestFocus()
         val imgr: InputMethodManager =
             requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imgr.showSoftInput(editTextTextPassword, InputMethodManager.SHOW_IMPLICIT)
 
+        // Adding listener to check pass/change view on enter click
         editTextTextPassword.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
+
+                // Hiding keyboard
+                val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm?.hideSoftInputFromWindow(view.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+
+                // Navigating to next view
                 findNavController().navigate(R.id.action_loginPassword_to_SecondFragment)
                 return@OnKeyListener true
             }
