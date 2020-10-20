f4bc20ce5498e1ea42cad9677d93996c7b3ede66
Edvin <Edvinbe@student.chalmers.se>
2020-10-20 11:51:14 +0200

    No meny and new background

--- app/src/main/java/com/example/workbalance/MainActivity.kt
+++ app/src/main/java/com/example/workbalance/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.workbalance
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        //setSupportActionBar(findViewById(R.id.toolbar))
 
     }
 
